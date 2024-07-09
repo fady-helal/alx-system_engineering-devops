0f899ca0b47399f8bc0f9af32f59c2e95a446e5e
fadyhelal <fadyhelal8@gmail.com>
2024-07-09 18:55:40 +0300

    Executable!

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# this script to puppet http header
+
+exec { 'update system':
+  command => '/usr/bin/apt-get update'
+}
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update system']
+}
+file { '/var/www/html/index.html':
+  content  => 'Hello world'
+}
+exec { 'redirect_me':
+  command  => 'sed -i "24i\    rewrite ^\/redirect_me https:\/\/github.com\/areejslama permanent;/" /etc/nginx/sites-enabled/default',
+  provider => 'shell'
+}
+exec { 'HTTP header':
+  command  => 'sed -i "25i\    add_header X-Served-By $HOSTNAME;/" /etc/nginx/sites-enabled/default',
+  provider => 'shell'
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
